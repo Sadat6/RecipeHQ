6e238e0580a70a1041dced79ef578445aee7e753
yaneyalvarado <alvyaney@gmail.com>
2023-04-03 19:28:43 -0600

    added api key to js

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -22,3 +22,28 @@ $(function mainPageSlide () {
     });
 });
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '51a8b8f970msh294e2129279c3a5p130ec8jsna317748ab7a8',
+		'X-RapidAPI-Host': 'worldwide-recipes1.p.rapidapi.com'
+	}
+};
+
+fetch('https://worldwide-recipes1.p.rapidapi.com/api/search?q=beef', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '51a8b8f970msh294e2129279c3a5p130ec8jsna317748ab7a8',
+// 		'X-RapidAPI-Host': 'worldwide-recipes1.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://worldwide-recipes1.p.rapidapi.com/api/explore', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
