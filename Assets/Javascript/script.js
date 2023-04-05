a0ab4618d7a54d0edd66c6b1b29bf9484d078fa3
Joseph Teklu <jteklu@outlook.com>
2023-04-04 21:28:44 -0600

    added the youtube api and some html to show on webpage

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -54,4 +54,63 @@ $(function mainPageSlide () {
     });
 });
 
+// youtube api
+// authinecating the google account
+//   function authenticate() {
+//     return gapi.auth2.getAuthInstance()
+//         .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+//         .then(function() { console.log("Sign-in successful"); },
+//               function(err) { console.error("Error signing in", err); });
+//   }
+// //   setting up key
+//   function loadClient() {
+//     gapi.client.setApiKey("AIzaSyAcGfs0ivOla4i7uh_q0nYkcjEgEADgwNA");
+//     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+//         .then(function() { console.log("GAPI client loaded for API"); },
+//               function(err) { console.error("Error loading GAPI client for API", err); });
+//   }
+//   // Make sure the client is loaded and sign-in is complete before calling this method.
+//   function execute() {
+//     // to search using the api make sure the key is inserted
+//     // use the (q) query to search for the desired video
+//     // make sure the (type) query is set to video 
+//     // make sure the (part) query is set to snippet
+//     return gapi.client.youtube.search.list({
+//       "part": ["snippet"],
+//       "q": "food",
+//       "type": ["video"]
+//     })
+//         .then(function(response) {
+//                 // Handle the results here (response.result has the parsed body).
+//                 console.log("Response", response);
+//               },
+//               function(err) { console.error("Execute error", err); });
+//   }
+//   gapi.load("client:auth2", function() {
+//     gapi.auth2.init({client_id: "789422668946-js76l4sad0pchts9nkvugsaum5da8cle.apps.googleusercontent.com"});
+//   });
 
+  
+
+function videoSearch(API_KEY, toSearch)
+{
+    // call the youtube api and search for the video given the query
+    $.get("https://www.googleapis.com/youtube/v3/search?key="+ API_KEY +"&type=video&part=snippet&q=" + toSearch, (data) => {
+        // output the response
+        console.log(data)
+
+        var video = ''
+
+        data.items.forEach(item => {
+            // grab the videos id from the html
+            // var video = $("#videos");
+
+            video = `<iframe width="420" height "315" src="http://youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscrean> </iframe>`
+
+            $("#videos").append(video);
+
+        })
+    });
+}
+
+videoSearch("AIzaSyAcGfs0ivOla4i7uh_q0nYkcjEgEADgwNA", "indian");
