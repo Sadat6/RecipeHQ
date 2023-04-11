66bf0d049fb9cdd507cce84f5d3dfd9e431c09b8
Joseph Teklu <jteklu@outlook.com>
2023-04-10 19:22:12 -0600

    fixed some youtube and html bugs

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -46,11 +46,11 @@ searchBtn.on('click', function(event){
                 foodList.append(foodListElement);
                 // foodList.append(foodListBrk);
 
-
-
                 $('#recipe').css('display','none')
                 $('#image').css('display','none')
                 $('#video').css('display','none')
+                $('.youtube-video').css('display','none')
+                              
 
             }     
             console.log("Num of food list buttons: " + $(".food-list-btn").length)
@@ -62,6 +62,7 @@ searchBtn.on('click', function(event){
                 $('#recipe').css('display','block')
                 $('#image').css('display','block')
                 $('#video').css('display','block')
+                $('.youtube-video').css('display','block')
             
                 indexBtnClicked = $(this).index();
 
@@ -70,6 +71,11 @@ searchBtn.on('click', function(event){
                 //Ingredient List
                 var foodIngredientElement = $('<li>');
                 foodIngredientElement.attr('class','food-ingredients');
+                
+                // call up youtube video
+                videoSearch("AIzaSyBFeYws43rpN5DIOd-G1ECcF1zxSwXR_yU", categorySelectedValue+"_how_to_make");
+
+
                 foodIngredientElement.text(data.hits[indexBtnClicked].recipe.ingredientLines);
 
                 //removing existing li element from recipe section
@@ -144,7 +150,40 @@ function videoSearch(API_KEY, toSearch)
     })
 }
 
-videoSearch("AIzaSyAcGfs0ivOla4i7uh_q0nYkcjEgEADgwNA", "ethiopian_food");
+// <![CDATA[  <-- For SVG support
+if ('WebSocket' in window) {
+    (function () {
+        function refreshCSS() {
+            var sheets = [].slice.call(document.getElementsByTagName("link"));
+            var head = document.getElementsByTagName("head")[0];
+            for (var i = 0; i < sheets.length; ++i) {
+                var elem = sheets[i];
+                var parent = elem.parentElement || head;
+                parent.removeChild(elem);
+                var rel = elem.rel;
+                if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+                    var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+                    elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+                }
+                parent.appendChild(elem);
+            }
+        }
+        var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+        var address = protocol + window.location.host + window.location.pathname + '/ws';
+        var socket = new WebSocket(address);
+        socket.onmessage = function (msg) {
+            if (msg.data == 'reload') window.location.reload();
+            else if (msg.data == 'refreshcss') refreshCSS();
+        };
+        if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+            console.log('Live reload enabled.');
+            sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+        }
+    })();
+}
+else {
+    console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+}
 
 
 
